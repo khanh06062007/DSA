b318c5916dc8cadde092c5309e52200ce65b79b4
Khanh <khanhphan662007@gmail.com>
2026-03-18 17:11:53 +0700

    E02

--- 25120074_PhanLeTuanKhanh_W01/W01_E02/main.cpp
+++ 25120074_PhanLeTuanKhanh_W01/W01_E02/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int n;
 string s;
 MyStack st;
-int main(){
+void module2_2(){
     cin>>s;
     n=s.size();
     initStack(st);
@@ -24,5 +24,38 @@ int main(){
     if(st.size==0)cout<<"true";
     else cout<<"false";
     clearStack(st);
+    cout<<'\n';
+}
+void module2_3(){
+    cin>>s;
+    n=s.size();
+    int res=0;
+    MyStack pos;
+    initStack(st);
+    initStack(pos);
+    push(pos,-1);
+    for(int i=0;i<n;i++){
+        int k=s[i]-0;
+        if(k==40){
+            push(st,k);
+            push(pos,i);
+        }
+        else if(!isEmpty(st) && top(st)==k-1){
+            pop(st);
+            pop(pos);
+            res=max(res,i-top(pos));
+        }
+        else{
+            push(st,k);
+            push(pos,i);
+        }
+    }
+    cout<<res;
+    clearStack(st);
+    clearStack(pos);
+}
+int main(){
+    module2_2();
+    module2_3();
     return 0;
 }
